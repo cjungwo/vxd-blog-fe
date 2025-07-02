cb69592af9be22e9ad564a4b91d50231606be161
cjungwo <jwoo516@naver.com>
2025-07-02 12:47:14 +1000

    #14-deploy remove deploy part in actions

--- next.config.ts
+++ next.config.ts
@@ -8,11 +8,8 @@ const nextConfig: NextConfig = {
     }
   },
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
-  output: 'export',
 };
 
 export default nextConfig;
