e1b4036991a1b6d565d1c7d09360e0acda87aa66
cjungwo <jwoo516@naver.com>
2025-07-05 16:48:52 +1000

    #14-deploy remove __dev__

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,15 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  webpack: (config, { isServer, dev }) => {
+    if (!dev) {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        '__dev__': false
+      };
+    }
+    return config;
+  }
 };
 
 export default nextConfig;
